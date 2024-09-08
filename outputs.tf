5b8310bbc91bcbd797bf3177b8ee0f2fd3c16d92
Sumesh Kumar Panda <sumeshpanda.k@gmail.com>
2024-09-08 17:42:57 +0530

    first commit

---
+++ outputs.tf
@@ -0,0 +1,11 @@
+output "control_plane_ip" {
+  value = aws_instance.kubeadm_demo_control_plane.public_ip
+}
+
+output "worker_nodes_ip" {
+  value = join("", aws_instance.kubeadm_demo_worker_nodes[*].public_ip)
+}
+
+# output "ami_id_from_data" {
+#   value = data.aws_ami.ubuntu.id
+# }
